0e266917cc158aa0cfa9416371452dc0cd2ace91
Mazen Amir <49759150+Selvster@users.noreply.github.com>
2023-08-31 00:58:16 +0300

    Announcments Skeleton

---
+++ database/migrations/2023_08_30_215636_create_announcments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('announcments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('classroom_id');
+            $table->foreign('classroom_id')->references('id')->on('classrooms')->onDelete('cascade');
+            $table->string('title', 100);
+            $table->string('description', 1000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('announcments');
+    }
+};
