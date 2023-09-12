111ce192c0a20c40f5365d351a5bfb07685219d6
Mazen Amir <49759150+Selvster@users.noreply.github.com>
2023-09-12 13:20:56 +0300

    Fixed Routes Including [Logical]

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@
 // Auth Routes
 include_once  'api_routes/auth.php';
 // Classrooms Routes
-include_once  'api_routes/classrooms/teacher.php';
-include_once  'api_routes/classrooms/student.php';
+include_once  'api_routes/teacher.php';
+include_once  'api_routes/student.php';
